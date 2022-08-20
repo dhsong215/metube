dca538942f01ea10c544a041cca3bd0246ccd6f8
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2022-08-21 01:49:23 +0900

    middleware and get,res

--- src/server.js
+++ src/server.js
@@ -5,3 +5,14 @@ const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`listening on PORT${PORT}`));
+
+const middleware = (req, res, next) => {
+  console.log("middleware working");
+  next();
+};
+
+const controller = (req, res) => {
+  res.send("controller respond");
+};
+
+app.get("/", middleware, controller);
