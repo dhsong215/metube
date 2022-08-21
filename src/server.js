018c341dd66b4c115466126811ca8e7e41bf71e4
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2022-08-21 16:33:10 +0900

    Add global middleware

--- src/server.js
+++ src/server.js
@@ -6,8 +6,16 @@ const PORT = 4000;
 
 app.listen(PORT, () => console.log(`listening on PORT${PORT}`));
 
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url == "protected") {
+    res.send("protected site");
+  }
+  next();
+};
+
 const middleware = (req, res, next) => {
-  console.log("middleware working");
+  console.log(`${req.method},${req.url}`);
   next();
 };
 
@@ -15,4 +23,7 @@ const controller = (req, res) => {
   res.send("controller respond");
 };
 
+app.use(privateMiddleware);
+
 app.get("/", middleware, controller);
+app.get("/protected", middleware, controller);
