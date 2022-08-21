0b0346cd8edcf3e2426c6b5b49d322d4b9d93c18
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2022-08-22 02:57:12 +0900

    external middleware
    add morgan

--- src/server.js
+++ src/server.js
@@ -1,29 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
 
 const PORT = 4000;
 
 app.listen(PORT, () => console.log(`listening on PORT${PORT}`));
 
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url == "protected") {
-    res.send("protected site");
-  }
-  next();
-};
-
-const middleware = (req, res, next) => {
-  console.log(`${req.method},${req.url}`);
-  next();
-};
-
 const controller = (req, res) => {
-  res.send("controller respond");
+  res.send("hi");
 };
 
-app.use(privateMiddleware);
-
-app.get("/", middleware, controller);
-app.get("/protected", middleware, controller);
+app.use(logger);
+app.get("/", controller);
+app.get("/protected", controller);
