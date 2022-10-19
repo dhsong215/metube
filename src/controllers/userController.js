62fdf3285cf4d7609e89639988ce13cfc1c9bf73
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2022-10-19 13:32:39 +0900

    social login (github)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
 import User from "../models/User";
 
@@ -46,6 +47,12 @@ export const postLogin = async (req, res) => {
     const errorMessage = "email doesn't exist";
     return res.status(400).render("login", { pageTitle, errorMessage });
   }
+  if (user.socialOnly == true) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "this email can only login for github",
+    });
+  }
   const passwordCorrect = await bcrypt.compare(password, user.password);
   if (!passwordCorrect) {
     return res
@@ -59,4 +66,79 @@ export const postLogin = async (req, res) => {
 
 export const logout = (req, res) => {
   res.send("this will be user remove page");
+};
+
+//깃헙 로그인시 controller의 config에 맞춰 url생성
+export const githubLogin = (req, res) => {
+  const config = {
+    client_id: process.env.CL_ID,
+    allow_signup: "false",
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  return res.redirect(`https://github.com/login/oauth/authorize?${params}`);
+};
+
+export const githubLoginFinish = async (req, res) => {
+  const config = {
+    client_id: process.env.CL_ID,
+    client_secret: process.env.CL_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalURL = `https://github.com/login/oauth/access_token?${params}`;
+  const tokenRequest = await (
+    await fetch(finalURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if (!("access_token" in tokenRequest)) {
+    return res.redirect("/login");
+  }
+  const { access_token } = tokenRequest;
+  const userData = await (
+    await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `token ${access_token}`,
+      },
+    })
+  ).json();
+  console.log(userData);
+  const emailData = await (
+    await fetch("https://api.github.com/user/emails", {
+      headers: {
+        Authorization: `token ${access_token}`,
+      },
+    })
+  ).json();
+  const emailObj = await emailData.find(
+    (email) => email.primary === true && email.verified === true
+  );
+  if (!emailObj) {
+    return res.redirect("/login", {
+      errorMessage: "there is no email on your github account,",
+    });
+  }
+  const userExisting = await User.findOne({
+    email: emailObj.email,
+  });
+  if (userExisting) {
+    req.session.loggedIn = true;
+    req.session.user = userExisting;
+    return res.redirect("/");
+  } else {
+    await User.create({
+      email: emailObj.email,
+      username: userData.login,
+      name: userData.login,
+      password: "",
+      location: userData.location,
+      socialOnly: true,
+      avatarURL: userData.avatarURL,
+    });
+    return res.redirect("/");
+  }
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,10 +4,14 @@ import {
   userEdit,
   userRemove,
   getProfile,
+  githubLogin,
+  githubLoginFinish,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
+userRouter.get("/githubLogin", githubLogin);
+userRouter.get("/github/callback", githubLoginFinish);
 
 export default userRouter;
